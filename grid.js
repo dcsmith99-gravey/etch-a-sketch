8091802118a3ef35ab8c3791ef0fd005c7805557
David Smith <dcsmith999@gmail.com>
2025-09-01 19:12:30 -0400

    Grid created using html, css, javascript

---
+++ grid.js
@@ -0,0 +1,14 @@
+const grid = document.getElementById("grid");
+
+for (let row = 0; row < 16; row++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("row");
+
+    for (let col = 0; col < 16; col++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        rowDiv.appendChild(cell);
+    }
+
+    grid.appendChild(rowDiv);
+}
